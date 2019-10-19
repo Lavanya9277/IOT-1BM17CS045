d252d838381897e8bc13317d677d7fb507e7e0eb
Lavanya9277 <53854535+Lavanya9277@users.noreply.github.com>
2019-10-19 13:13:45 +0530

    Add files via upload

---
+++ gsm.ino.ino
@@ -0,0 +1,16 @@
+#include<SoftwareSerial.h>
+SoftwareSerial cell(2,3);
+void setup() {
+  // put your setup code here, to run once:
+   cell.begin(9600);
+   delay(500);
+   Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("calling...");
+  cell.println("ATD+9535434768;");
+  delay(500);
+  cell.println("ATH");
+}
